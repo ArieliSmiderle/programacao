78829b26d4c91f63fbed1b8369f163143198728c
Arieli Rosa <arieli.rosa@monacobr.com.br>
2022-12-26 09:18:28 -0300

    novos codigos

---
+++ extrator-url.py
@@ -0,0 +1,27 @@
+#url = "https://bytebank.com/cambio?quantidade=100&moedaDestino=dolar&moedaOrigem=real"
+url = ""
+
+#sanitização da URL
+url = url.replace(" ", "")
+
+#validação da URL
+if url == "":
+    raise ValueError("A URL está vazia")
+
+#separa base e os parâmetros
+indice_interrogacao = url.find('?')
+url_base = url[:indice_interrogacao]
+url_parametros = url[indice_interrogacao+1:]
+print(url_parametros)
+
+#busca o valor de um parâmetro
+parametro_busca = 'moedaDestino'
+indice_parametro = url_parametros.find(parametro_busca)
+indice_valor = indice_parametro + len(parametro_busca) + 1
+indice_e_comercial = url_parametros.find('&', indice_valor)
+if indice_e_comercial == -1:
+    valor = url_parametros[indice_valor:]
+else:
+    valor = url_parametros[indice_valor:indice_e_comercial]
+    
+print(valor)
---
+++ introducao_as_colecoes_do_python.py
@@ -0,0 +1,11 @@
+idade1 = 39
+idade2 = 30
+idade3 = 27
+idade4 = 18
+
+print(idade1)
+print(idade2)
+print(idade3)
+print(idade4)
+
+idades = [39, 30, 27, 1, 8]
